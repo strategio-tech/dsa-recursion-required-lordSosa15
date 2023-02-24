776ba569b8accf8ce5f20d6ec6b91efd4d786811
lordSosa15 <97013669+lordSosa15@users.noreply.github.com>
2023-02-24 15:31:13 -0500

    completed

--- src/main/java/tech/strategio/E01.java
+++ src/main/java/tech/strategio/E01.java
@@ -3,8 +3,21 @@ package tech.strategio;
 import java.util.*;
 
 public class E01 {
-    public int getNthFib(int n) {
-        // FIXME
-        return -1;
+    public static int getNthFib(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 0;
+        } else if (n == 2) {
+            return 1;
+        } else {
+            int[] fib = new int[n];
+            fib[0] = 0;
+            fib[1] = 1;
+            for (int i = 2; i < n; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
+            }
+            return fib[n - 1];
+        }
     }
 }
